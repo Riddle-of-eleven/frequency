fb0c7e70f57447227d31556c9817f9bef9c1bb54
user <nikavorosh@gmail.com>
2025-02-03 03:02:04 +0300

    Mexh

--- analyse.py
+++ analyse.py
@@ -4,11 +4,19 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import utility as u
 
-file = 'images/small_circles2.png'
+file = 'images/v_long_small.png'
+# file = 'images/small_circles.png'
 # file = 'images/chips.jpg'
+# file = 'images/pattern.jpg'
+# file = 'images/cat.jpg'
+# file = 'images/cat (posterization).jpg'
 
-row_num = 13
+row_num = 1   # очень маленькие
+# row_num = 13  # кружочки
 # row_num = 1
+# row_num = 130   # чипсы
+# row_num = 380   # паттерн
+# row_num = 420   # котик
 
 image = Image.open(file).convert('L')   # грейскейл
 image = np.array(image)
@@ -20,7 +28,11 @@ row = image[row_num]
 # plt.imshow(image, cmap='gray', vmin=0, vmax=255)
 # plt.show()
 
-# u.show_wavelet(row)
-u.discrete_wavelet(row)
+u.show_wavelet(row)
+# u.discrete_wavelet(row)
 
 # построить комплексную карту для изображения целиком
+
+####
+
+# print(u.get_large_scale(row))
--- utility.py
+++ utility.py
@@ -5,17 +5,27 @@ from PIL import Image
 
 
 def show_wavelet(row):
-    scales = np.arange(1, 20)
-    coefficients, frequencies = pywt.cwt(row, scales, 'morl')
+    range_numbers = [0.1, 2]
+    scales = np.arange(range_numbers[0], range_numbers[1])
+    coefficients, frequencies = pywt.cwt(row, scales, 'mexh')  # вейвлет мексиканская шляпа
     # print(coefficients, frequencies)
 
     plt.figure()
-    plt.imshow(coefficients, aspect='auto', extent=[0, len(row), scales[0], scales[1]], cmap='binary')
+    plt.imshow(coefficients, aspect='auto', extent=[0, len(row), scales[0], scales[-1]], cmap='binary')
     plt.colorbar(label="Амплитуда")
     plt.ylabel("Масштаб")
     plt.xlabel("Пиксели")
     plt.title("Вейвлет-спектр")
     plt.show()
+
+def get_large_scale(row):
+    low = 1
+    high = 20
+    scales = np.arange(low, high)
+    coefficients, frequencies = pywt.cwt(row, scales, 'mexh')
+    return coefficients[-1:, :]
+
+
 
 def show_graph(row):
     # отображение ряда в виде графика
